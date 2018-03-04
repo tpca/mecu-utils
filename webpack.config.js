67d20f7051a535ce62d2265338870717f10c9977
Christian Dallago <code@dallago.us>
2018-03-04 10:08:43 -0500

    make windows proof

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     entry: __dirname + '/lib/index.js',
     output: {
-         path: __dirname + '/build',
-         filename: 'mecu-utils.js',
-         libraryTarget: 'umd',
-         library: 'MecuUtils'
+        path: __dirname + '/build',
+        filename: 'mecu-utils.js',
+        libraryTarget: 'umd',
+        library: 'MecuUtils',
+        // Follow https://github.com/webpack/webpack/issues/6522
+        globalObject: 'this'
     }
- };
+};
